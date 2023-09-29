846a356bed902787c187e2ffca878ab94aa05c07
NovaSquirrel <bushytail@novasquirrel.com>
2023-09-29 13:16:40 -0400

    Fix cmake config

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# Run this from the build directory
+export CMAKE_C_COMPILER=/bin/arm-none-eabi-gcc
+export CMAKE_CXX_COMPILER=/bin/arm-none-eabi-g++
+export PICO_SDK_PATH=/home/nova/pico/pico-sdk
+cmake ..
